a6f0234d0c4c7a845d70af7922d9cad7f986ae72
Alex <windcrack@mail.ru>
2018-11-03 13:21:24 +0300

    Tabs

--- js/sript.js
+++ js/sript.js
@@ -1,8 +1,9 @@
 window.addEventListener('DOMContentLoaded', function () {
   'use strict';
-
+  // Window Tab
   let glazTab = document.querySelectorAll('.link-tab'),
       glBlock = document.querySelector('.glazing_slider'),
+      body = document.querySelector('body'),
       glazContent = document.querySelectorAll('.glaz-content');
       function hideGlaz(h) {
         for(let i = h; i < glazContent.length; i++){
@@ -20,17 +21,46 @@ window.addEventListener('DOMContentLoaded', function () {
           glazTab[s].classList.add('active');
         }
       }
+    // Decoration Tab
+    let decTab = document.querySelectorAll('.dec-tab'),
+        noClick = document.querySelectorAll('.no_click'),
+        decorContent = document.querySelectorAll('.decor-content');
+      function hideDecor(h) {
+        for (let i = h; i < decorContent.length; i++) {
+          decorContent[i].classList.remove('show');
+          decorContent[i].classList.add('hide');
+          noClick[i].classList.remove('after_click');
+        }
+      }
+      hideDecor(1);
 
-      glBlock.addEventListener('click', function (e) {
-        let target = e.target;
-            if (target && target.classList.contains('link-tab')) {
-              for (let i = 0; i < glazTab.length; i++) {
-                if (target == glazTab[i]) {
-                  hideGlaz(0);
-                   showGlaz(i);
-                   break;
-                }
-              }
-            }
-        });
+      function showDecor(s) {
+        if (decorContent[s].classList.contains('hide')) {
+          decorContent[s].classList.remove('hide');
+          decorContent[s].classList.add('show');
+          noClick[s].classList.add('after_click');
+        }
+      }
+  body.addEventListener('click', function (e) {
+    let target = e.target;
+    if (target && target.classList.contains('link-tab')) {
+      for (let i = 0; i < glazTab.length; i++) {
+        if (target == glazTab[i]) {
+          hideGlaz(0);
+          showGlaz(i);
+          break;
+        }
+      }
+    }
+    if (target && target.classList.contains('dec-tab')) {
+      for (let i = 0; i < decTab.length; i++) {
+        if (target == decTab[i]) {
+          hideDecor(0);
+          showDecor(i);
+          break;
+        }
+      }
+    }
+    console.log(target);
+  });
 });
